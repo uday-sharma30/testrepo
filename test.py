a31b81e580a66d80117300d0d635a776f7b10135
uday sharma <118256723+uday-sharma30@users.noreply.github.com>
2023-06-20 22:48:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
 print("This is in child file so it will not affect any changes in the main file while we merge then we can see the 
 changes in the main file also,child file is created so we can test or experiment with our code")
+
+after that we can pull req and can merge the two files after merging child_branch and the main branch(master branch) we can delete the 
+child branch and can see the file is now present in the main branch
